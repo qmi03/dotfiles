583aa4c22134f0e9942445b4901a6172dc9e73ad
Qmi <minh.pham1126@gmail.com>
2024-10-19 21:01:02 +0700

    wezterm: change font

--- .config/wezterm/config/fonts.lua
+++ .config/wezterm/config/fonts.lua
@@ -2,7 +2,7 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
-local font_family = 'JetBrainsMono Nerd Font'
+local font_family = 'JetBrainsMonoNL Nerd Font'
 local font_size = platform.is_mac and 18 or 9
 
 return {
