a3efe98dac6715072e2ddbc5c67be6f6dd8e7b4e
Qmi <minh.pham1126@gmail.com>
2024-10-19 20:42:15 +0700

    wezterm: update font

--- .config/wezterm/config/appearance.lua
+++ .config/wezterm/config/appearance.lua
@@ -1,6 +1,7 @@
 local wezterm = require('wezterm')
 local gpu_adapters = require('utils.gpu_adapter')
 local colors = require('colors.custom')
+local fonts = require('config.fonts')
 
 return {
    animation_fps = 60,
@@ -48,8 +49,8 @@ return {
    },
    window_frame = {
       active_titlebar_bg = '#090909',
-      -- font = fonts.font,
-      -- font_size = fonts.font_size,
+      font = fonts.font,
+      font_size = fonts.font_size,
    },
    -- inactive_pane_hsb = {
    --    saturation = 0.9,
