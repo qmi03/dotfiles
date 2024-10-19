81617642672395d91ed2f4549ad80bdf8cae2f56
Qmi <minh.pham1126@gmail.com>
2024-10-19 20:28:16 +0700

    added (wezterm): some guys on github wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,19 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local Config = require('config')
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+require('utils.backdrops')
+   :set_files()
+   -- :set_focus('#000000')
+   :random()
 
--- This is where you actually apply your config choices
+require('events.right-status').setup()
+require('events.left-status').setup()
+require('events.tab-title').setup()
+require('events.new-tab-button').setup()
 
--- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Mocha'
-
--- and finally, return the configuration to wezterm
-return config
+return Config:init()
+   :append(require('config.appearance'))
+   :append(require('config.bindings'))
+   :append(require('config.domains'))
+   :append(require('config.fonts'))
+   :append(require('config.general'))
+   :append(require('config.launch')).options
