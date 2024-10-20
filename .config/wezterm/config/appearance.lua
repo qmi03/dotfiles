32e78ac7f13a8e16771ed2b98180a2847ea98263
Qmi <minh.pham1126@gmail.com>
2024-10-20 14:21:56 +0700

    wezterm: remove paddings

--- .config/wezterm/config/appearance.lua
+++ .config/wezterm/config/appearance.lua
@@ -44,8 +44,8 @@ return {
    window_padding = {
       left = 0,
       right = 0,
-      top = 10,
-      bottom = 7.5,
+      top = 0,
+      bottom = 0,
    },
    window_frame = {
       active_titlebar_bg = '#090909',
