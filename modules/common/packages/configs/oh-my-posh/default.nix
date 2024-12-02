dd640a18ded4075dba6169bf6e8d3809547b2352
Qmi <minh.pham1126@gmail.com>
2024-12-02 19:58:25 +0700

    remove oh my posh config

--- modules/common/packages/configs/oh-my-posh/default.nix
+++ modules/common/packages/configs/oh-my-posh/default.nix
@@ -1,21 +1,8 @@
-{ config,  ... }:
+{ config, ... }:
 {
   home-manager.users.${config.user} = {
-    home = {
-      file = {
-        ".config/oh-my-posh".source = ./themes;
-      };
-    };
-
     programs.oh-my-posh = {
       settings = builtins.fromTOML (builtins.unsafeDiscardStringContext (builtins.readFile "./themes/tokyonight_storm.qmi.toml"));
     };
-    ### TODO: Modify the home-manager source code for oh-my-posh to support other file type like toml
-    # programs.oh-my-posh = {
-    #   enable = true;
-    #   enableZshIntegration = true;
-    #   settings =
-    #     builtins.fromJSON (builtins.unsafeDiscardStringContext (builtins.readFile "''${pkgs.oh-my-posh}/share/oh-my-posh/themes/space.omp.json"));
-    # };
   };
 }
