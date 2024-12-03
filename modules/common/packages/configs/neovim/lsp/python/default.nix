6804c6b9c7657371f39d9f8aea7abf10bc305876
Qmi <minh.pham1126@gmail.com>
2024-12-03 23:32:11 +0700

    test new config with lsp

--- modules/common/packages/configs/neovim/lsp/python/default.nix
+++ modules/common/packages/configs/neovim/lsp/python/default.nix
@@ -1,8 +1,13 @@
-{ config, pkgs,... }:
+{ pkgs, ... }:
 {
-  home-manager.users.${config.user}.home.packages = with pkgs;[
-    python312Packages.python-lsp-server
-    ruff
-    mypy
-  ];
+  programs.neovim = {
+    plugins = with pkgs;[
+      python312Packages.python-lsp-server
+      ruff
+      mypy
+    ];
+    extraLuaConfig = ''
+      ${builtins.readFile ./python.lua}
+    '';
+  };
 }
