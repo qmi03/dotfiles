a59ceeb6492ab61543a1a032b4a83bd169f55675
Qmi <minh.pham1126@gmail.com>
2024-12-04 01:31:12 +0700

    nvim: update presence to ignore oil and update config inspiration

--- modules/common/packages/configs/neovim/default.nix
+++ modules/common/packages/configs/neovim/default.nix
@@ -36,27 +36,29 @@
       plugins = with pkgs.vimPlugins;[
         lazy-nvim
       ];
+      # See this for a more in depth explanation on how to make Lazy work together with Nix
+      # https://nixalted.com/
       extraLuaConfig = ''
-                require("vim_opts")
-                vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
-                require("lazy").setup({
-        	spec = {
-        	{ import = "plugins" },
-        	},
-                  performance = {
-                    reset_packpath = false,
-                    rtp = {
-                        reset = false,
-                      }
-                    },
-                  dev = {
-                    path = "${pkgs.vimUtils.packDir config.home-manager.users.${config.user}.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
-                  },
-                  install = {
-                    -- Safeguard in case we forget to install a plugin with Nix
-                    missing = false,
-                  },
-                })
+        require("vim_opts")
+        vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+        require("lazy").setup({
+        spec = {
+        { import = "plugins" },
+        },
+          performance = {
+            reset_packpath = false,
+            rtp = {
+                reset = false,
+              }
+            },
+          dev = {
+            path = "${pkgs.vimUtils.packDir config.home-manager.users.${config.user}.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+          },
+          install = {
+            -- Safeguard in case we forget to install a plugin with Nix
+            missing = false,
+          },
+        })
       '';
     };
     xdg.configFile."nvim/lua" = {
