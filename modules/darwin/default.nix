7aef0546d15b267f4366f58abc343d12f4a600ac
Qmi <minh.pham1126@gmail.com>
2024-12-05 22:23:41 +0700

    add some macos specific settings

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -3,9 +3,11 @@
     ../common
     ./configs
     ./homebrew.nix
+    ./environment.nix
     ./packages.nix
     ./system.nix
     ./touch-id.nix
     ./users.nix
+    ./networking.nix
   ];
 }
--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,24 +1,85 @@
 { config, pkgs, ... }: {
-  system.stateVersion = 5;
   nixpkgs.hostPlatform = "aarch64-darwin";
-  system.activationScripts.applications.text =
-    let
-      env = pkgs.buildEnv {
-        name = "system-applications";
-        paths = config.environment.systemPackages;
-        pathsToLink = "/Applications";
+  system = {
+    stateVersion = 5;
+    keyboard = {
+      remapCapsLockToControl = true;
+      enableKeyMapping = true;
+    };
+    activationScripts.applications.text =
+      let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+      pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+      '';
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleKeyboardUIMode = 3;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
       };
-    in
-    pkgs.lib.mkForce ''
-      # Set up applications.
-      echo "setting up /Applications..." >&2
-      rm -rf /Applications/Nix\ Apps
-      mkdir -p /Applications/Nix\ Apps
-      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-      while read -r src; do
-        app_name=$(basename "$src")
-        echo "copying $src" >&2
-        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-      done
-    '';
+
+      dock = {
+        enable-spring-load-actions-on-all-items = true;
+        mouse-over-hilite-stack = true;
+
+        mineffect = "genie";
+        orientation = "left";
+        show-recents = false;
+        tilesize = 44;
+
+        persistent-apps = [
+          "${pkgs.firefox-bin}/Applications/Firefox.app"
+          "${pkgs.kitty}/Applications/Kitty.app"
+          "/Applications/Microsoft Teams (work or school).app"
+          "/Applications/Microsoft Outlook.app"
+          "/Applications/Microsoft Remote Desktop.app"
+        ];
+      };
+
+      finder = {
+        FXPreferredViewStyle = "clmv";
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
+        QuitMenuItem = true;
+      };
+
+      LaunchServices.LSQuarantine = false;
+
+      CustomUserPreferences = {
+        "com.apple.desktopservices" = {
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.dock" = {
+          magnification = true;
+          largesize = 48;
+        };
+        "com.apple.finder" = {
+          WarnOnEmptyTrash = false;
+          FXDefaultSearchScope = "SCcf";
+          FXPreferredViewStyle = "clmv";
+        };
+      };
+    };
+  };
 }
