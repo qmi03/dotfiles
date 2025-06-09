2d10d0954c05c5bd6877896efa7c8ceb0125c75c
qmi <minh.pham1126@gmail.com>
2025-06-09 11:22:02 +0700

    rename config/nix to config/+nix

--- config/default.nix
+++ config/default.nix
@@ -1,17 +1,17 @@
 { ... }: {
   imports = [
-    ./nix/neovim
-    ./nix/btop
-    ./nix/zsh
-    ./nix/catppuccin
-    ./nix/oh-my-posh
-    ./nix/sioyek
-    ./nix/bat
-    ./nix/bottom
-    ./nix/direnv
-    ./nix/git
-    ./nix/ripgrep
-    ./nix/zathura
-    ./nix/spicetify
+    ./+nix/neovim
+    ./+nix/btop
+    ./+nix/zsh
+    ./+nix/catppuccin
+    ./+nix/oh-my-posh
+    ./+nix/sioyek
+    ./+nix/bat
+    ./+nix/bottom
+    ./+nix/direnv
+    ./+nix/git
+    ./+nix/ripgrep
+    ./+nix/zathura
+    ./+nix/spicetify
   ];
 }
