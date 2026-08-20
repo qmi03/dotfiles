f91ed06c11930985877139edc056358d0cc01163
qmi <minh.pham1126@gmail.com>
2026-08-20 23:41:45 +0700

    config(sioyek): reformat to fit new schema

--- config/+nix/sioyek/default.nix
+++ config/+nix/sioyek/default.nix
@@ -8,14 +8,20 @@
         "move_down" = "j";
         "move_left" = "h";
         "move_right" = "l";
-        "screen_down" = [ "d" "<C-d>" ];
-        "screen_up" = [ "u" "<C-u>" ];
+        "screen_down" = [
+          "d"
+          "<C-d>"
+        ];
+        "screen_up" = [
+          "u"
+          "<C-u>"
+        ];
       };
       config = {
         "background_color" = "0.0 0.0 0.0";
         "text_highlight_color" = "1.0 0.0 0.0";
         "should_launch_new_window" = "1";
-        "startup_commands" = "toggle_titlebar";
+        "startup_commands" = [ "toggle_titlebar" ];
       };
     };
   };
